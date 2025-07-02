3ae39e77b0c77378433f2267e57323a91949617f
harrypotter2211 <yellaharish4@gmail.com>
2025-07-02 12:20:25 +0530

    Update InvsalesProductServiceApplicationTests.java

--- src/test/java/com/invsales/product/InvsalesProductServiceApplicationTests.java
+++ src/test/java/com/invsales/product/InvsalesProductServiceApplicationTests.java
@@ -2,10 +2,12 @@ package com.invsales.product;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
 @ActiveProfiles("test")
 class InvsalesProductServiceApplicationTests {
+
     @Test
     void contextLoads() {
     }
